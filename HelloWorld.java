7115642b7c0730d820800e0ebf9d79b4a2a92e21
Goutham <gouthamkallempudi1996@gmail.com>
2019-01-05 12:10:39 +0530

    Hello World updated in brach1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		int a , b;
+		a = 10;
+		b = 0;
+		while(a>0){
+			b = b + 1;
+			a = a - 1;
+		}
+		System.out.println("b value is" + b);
 	} 
 }
